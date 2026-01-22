101668036aa6caf0e66de10060b8fcc2d0494f4c
ArthurMartinns <arthurmartiinscontato@gmail.com>
2026-01-22 10:50:30 -0300

    Chess layer

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,19 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+public class ChessPiece extends Piece{
+	private EColor color;
+	
+	public ChessPiece(Board board, EColor color) {
+		super(board);
+		this.color = color;	
+	}
+
+	public EColor getColor() {
+		return color;
+	}
+	
+	
+}
